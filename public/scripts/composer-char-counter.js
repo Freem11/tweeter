e597a0ceebfe1ef536f0d0fd48070dde8db08464
Freem11 <freem1985@gmail.com>
2021-10-05 14:40:05 -0700

    box shadow issues still

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  
+  let MaxCharacters = 140
+  $("#tweet-text").on('keyup', function() {
+    let tweetLength = $("#tweet-text").val().length
+    
+    let remainingCharacters = MaxCharacters - tweetLength
+
+    $("#counter").val(remainingCharacters)
+
+    if (remainingCharacters < 0){
+      $("#counter").css('color','red')
+    } else {
+       $("#counter").css('color','#545149')
+    }
+    // console.log(remainingCharacters); //The this keyword is a reference to the button
+  });
+
+  
+
+
+
+
+
+
+
+
+
+});
